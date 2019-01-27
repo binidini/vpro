9480359d4f9c7ba238d99a9913d2e52eaadb1f03
Denis Manilo <denism@kmslh.com>
2019-01-27 20:19:23 +0300

    changes on new theme

--- config/bundles.php
+++ config/bundles.php
@@ -57,7 +57,7 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['dev' => true, 'test' => true], // WYSIWYG editor
-    SitemapPlugin\SitemapPlugin::class => ['dev' => true, 'test' => true], // Sitemap support
-    BitBag\SyliusCmsPlugin\BitBagSyliusCmsPlugin::class  => ['dev' => true, 'test' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true], // WYSIWYG editor
+    SitemapPlugin\SitemapPlugin::class => ['all' => true], // Sitemap support
+    BitBag\SyliusCmsPlugin\BitBagSyliusCmsPlugin::class  => ['all' => true],
 ];
